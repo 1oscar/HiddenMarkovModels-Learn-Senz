1d5f05d695fee432461cd1a2c04ebf9fdc7a57c0
meowoodie <woodielove@163.com>
2015-01-21 10:12:08 +0800

    revise hmm input format

--- hmm_senz/hmm/hmm.py
+++ hmm_senz/hmm/hmm.py
@@ -3,23 +3,34 @@ __author__ = 'woodie'
 class HMM:
     '''Base Class : HMM
 
-    The base class is used to solve the HMM problem
+    The base class is used to solve the basic HMM problem.
     '''
     def __init__(self,
                  visible_output,
                  hidden_state,
-                 transition_init,
-                 emission__init):
+                 transition_init, # The Matrix of hidden states' transition probability
+                 emission__init): # The Matrix of visible outputs' emission probability
         self.hVisibleOutput = visible_output
         self.hHiddenState   = hidden_state
-        self.hTransitionP   = {}
-        self.hEmissionP     = {}
         self.hSizeHmm       = {
-            'visible state' : len(self.hVisibleOutput),
-            'hidden state'  : len(self.hHiddenState)
+            'visible output' : len(self.hVisibleOutput),
+            'hidden state'   : len(self.hHiddenState)
         }
+        # The initial of hTransitionP
+        # - It's a 2-dimension dict
+        # - { key1 : { key2 : value } }
+        # - key1 is hidden states' name
+        # - key2 is hidden states' name
+        # - value is the transition probability from key1 to key2
+        self.hTransitionP   = transition_init
+        # The initial of hEmissionP
+        # - It's a 2-dimension dict
+        # - { key1 : { key2 : value } }
+        # - key1 is hidden states' name
+        # - key2 is visible outputs' name
+        # - value is the transition probability from key1 to key2
+        self.hEmissionP     = emission__init
 
-        for vi in self.hVisibleOutput:
-            for vj in self.hVisibleOutput:
-                self.hTransitionP[vi][vj] = transition_init[i][j]
+
+
 
--- hmm_senz/test/hmm_printer.py
+++ hmm_senz/test/hmm_printer.py
@@ -4,7 +4,7 @@ from hmm_senz.hmm import hmm
 
 visible_output = ("HOT","COLD","FROZEN")
 hidden_state   = ("WINDY","SUNNY","RAINNY")
-transition_init = [[0.2,0.4,0.4],[0.3,0.3,0.4],[0.1,0.1,0.8]]
+transition_init = {'WINDY': {'WINDY': 0.2, 'SUNNY': 0.4, 'RAINNY': 0.4}, 'SUNNY': {'WINDY': 0.3, 'SUNNY': 0.3, 'RAINNY': 0.4}, 'RAINNY': {'WINDY': 0.1, 'SUNNY': 0.1, 'RAINNY': 0.8}}
 emission_init  = [[0.2,0.4,0.4],[0.3,0.3,0.4],[0.1,0.1,0.8]]
 HMM = hmm.HMM(
     visible_output,
@@ -12,7 +12,9 @@ HMM = hmm.HMM(
     transition_init,
     emission_init
 )
-
+print HMM.hTransitionP
+print HMM.hHiddenState
+print HMM.hVisibleOutput
 
 
 
